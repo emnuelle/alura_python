7d59558c833c721171358a9444f15148784fdd4b
emnuelle <rm97973@fiap.com.br>
2023-05-04 15:42:42 -0300

    random

---
+++ ex5.py
@@ -0,0 +1,41 @@
+# Gerando e arredondando um número (Tornado o número secreto aleatório e exibindo ele no final do programa)
+
+import random
+
+print("*********************************")
+print("Bem vindo ao jogo de adivinhação!")
+print("*********************************")
+
+numero_secreto = round(random.random() * 100)
+total_de_tentativas = 3
+rodada = 1
+
+for rodada in range(1, total_de_tentativas + 1) :
+    
+    print(f"Tentativa {rodada} de {total_de_tentativas}")
+    chute_str = input("Digite o seu número entre 1 e 100: ")
+    print(f"Você digitou: {chute_str}")
+    chute = int(chute_str)   
+
+    if(chute <  1 or chute > 100):
+        print("Você deve digitar um número ente 1 e 100!")
+        continue 
+    # continue tipo o break mas continua
+
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if(acertou):
+        print("Você acertou!")
+        break
+    # Break não seria o ideal, mas deu certo e por enquanto só sei assim :) {No fim o professor do curso usou o break rssss}
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
+
+    rodada = rodada + 1
+
+print(f"A resposta correto é {numero_secreto}, Fim do jogo")
